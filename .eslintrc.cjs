499293c9db29d8c30ff6350542a23a44cd8f5ba1
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2024-01-10 23:11:31 -0600

    Updated the settings and details of the repo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,31 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript'
-  ],
+  extends: ["plugin:vue/vue3-essential", "plugin:vuetify/base", "eslint:recommended", "@vue/eslint-config-typescript"],
+  plugins: ["prettier", "@typescript-eslint"],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: "latest"
+  },
+  rules: {
+    quotes: ["warn", "double"],
+    semi: ["warn", "always"],
+    "prettier/prettier": [
+      "warn",
+      {
+        semi: true,
+        trailingComma: "none",
+        singleQuote: false,
+        printWidth: 120
+      }
+    ],
+    "comma-dangle": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "vue/multiline-html-element-content-newline": "off",
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/no-unused-vars": "warn",
+    "vue/html-self-closing": "off"
   }
-}
+};
