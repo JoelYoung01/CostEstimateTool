0dbb8dba0a8c11d06c81b1a93730c9ce79fa9d39
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2024-01-18 01:20:40 -0600

    Updated contact page to look decent on mobile

--- src/components/CollectContactInfo.vue
+++ src/components/CollectContactInfo.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import type { ContactInfo } from "@/types";
+import { useDisplay } from "vuetify";
 
 const emit = defineEmits<{
   confirm: [data: ContactInfo];
@@ -20,6 +21,8 @@ const contactInfo = ref<ContactInfo>({
   desiredCompletionDate: nextWeek()
 });
 
+const { mobile } = useDisplay();
+
 const validForm = ref(false);
 const showDatePicker = ref(false);
 
@@ -35,73 +38,70 @@ const daysFromToday = (date: Date) => {
 </script>
 
 <template>
-  <v-card flat>
-    <v-card-title class="text-center mb-2" style="font-size: 24px; font-weight: bolder">
-      We need a few more details from you<br />
-      to calculate your estimate
-    </v-card-title>
+  <v-card flat class="px-lg-8 px-4">
+    <div class="text-h5 text-center mb-2" style="font-size: 24px; font-weight: bolder">
+      {{ mobile ? "Enter a few more details" : "We need a few more details from you to calculate your estimate" }}
+    </div>
 
-    <v-card-text>
-      <v-form v-model="validForm">
-        <v-text-field
-          v-model="contactInfo.name"
-          prepend-icon="mdi-account"
-          label="Name"
-          variant="outlined"
-          class="mb-3"
-          :rules="[required]"
-        />
-        <v-text-field
-          v-model="contactInfo.email"
-          prepend-icon="mdi-email"
-          validate-on="blur"
-          label="Email"
-          placeholder="johndoe@gmail.com"
-          variant="outlined"
-          class="mb-3"
-          :rules="[required, isEmail]"
-        />
-        <v-text-field
-          v-model="contactInfo.phone"
-          prepend-icon="mdi-phone"
-          label="Phone"
-          variant="outlined"
-          class="mb-3"
-          :rules="[required]"
-        />
-        <v-text-field
-          :modelValue="contactInfo.desiredCompletionDate.toDateString()"
-          readonly
-          prepend-icon="mdi-calendar-month-outline"
-          label="Desired Completion Date"
-          variant="outlined"
-          class="mb-3"
-          :rules="[required]"
-        >
-          <v-menu v-model="showDatePicker" activator="parent" :close-on-content-click="false">
-            <v-date-picker
-              v-model="contactInfo.desiredCompletionDate"
-              :min="new Date()"
-              landscape
-              @update:modelValue="showDatePicker = false"
-            />
-          </v-menu>
+    <v-form v-model="validForm">
+      <v-text-field
+        v-model="contactInfo.name"
+        prepend-icon="mdi-account"
+        label="Name"
+        variant="outlined"
+        class="mb-3"
+        :rules="[required]"
+      />
+      <v-text-field
+        v-model="contactInfo.email"
+        prepend-icon="mdi-email"
+        validate-on="blur"
+        label="Email"
+        placeholder="johndoe@gmail.com"
+        variant="outlined"
+        class="mb-3"
+        :rules="[required, isEmail]"
+      />
+      <v-text-field
+        v-model="contactInfo.phone"
+        prepend-icon="mdi-phone"
+        label="Phone"
+        variant="outlined"
+        class="mb-3"
+        :rules="[required]"
+      />
+      <v-text-field
+        :modelValue="contactInfo.desiredCompletionDate.toDateString()"
+        readonly
+        prepend-icon="mdi-calendar-month-outline"
+        label="Desired Completion Date"
+        variant="outlined"
+        class="mb-3"
+        :rules="[required]"
+      >
+        <v-menu v-model="showDatePicker" activator="parent" :close-on-content-click="false">
+          <v-date-picker
+            v-model="contactInfo.desiredCompletionDate"
+            :min="new Date()"
+            landscape
+            @update:modelValue="showDatePicker = false"
+          />
+        </v-menu>
 
-          <template #append-inner>
-            <span style="font-size: 12px; white-space: nowrap">
-              In {{ daysFromToday(contactInfo.desiredCompletionDate) }} day{{
-                daysFromToday(contactInfo.desiredCompletionDate) === 1 ? "" : "s"
-              }}
-            </span>
-          </template>
-        </v-text-field>
-      </v-form>
-    </v-card-text>
+        <template #append-inner>
+          <span style="font-size: 12px; white-space: nowrap">
+            In {{ daysFromToday(contactInfo.desiredCompletionDate) }} day{{
+              daysFromToday(contactInfo.desiredCompletionDate) === 1 ? "" : "s"
+            }}
+          </span>
+        </template>
+      </v-text-field>
+    </v-form>
 
-    <v-card-actions>
-      <v-btn color="primary" @click="emit('back')"> Back </v-btn>
+    <div class="d-flex mx-n2">
+      <v-btn slim variant="text" @click="emit('back')"> Back </v-btn>
       <v-spacer />
-      <v-btn color="primary" @click="emit('confirm', contactInfo)" :disabled="!validForm"> Confirm </v-btn>
-    </v-card-actions>
+      <v-btn slim variant="text" @click="emit('confirm', contactInfo)" :disabled="!validForm"> Confirm </v-btn>
+    </div>
   </v-card>
 </template>
